646f81d091a7f7f46badf9cf5394f1cf6a196813
Shaypher Ashe <149912769+shaypheraddison@users.noreply.github.com>
2024-01-21 15:29:07 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -11,10 +11,16 @@ app = FastAPI()
 
 if path.is_file() == False:
     with open(path, "a+") as writer:
-        writer.write("")
+        writer.write("[\n\t\n]")
 else:
     with open(path, "a+") as editor:
         editor.read()
+
+class To_Do(BaseModel):
+    id: Optional[str] | None = None
+    title: str
+    message: str
+    is_completed: bool
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -22,12 +28,6 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 def gen_rando_id():
     random_id = (''.join(random.choices(letters, k=2) + random.choices(numbers, k=2)))
     return random_id
-
-class To_Do(BaseModel):
-    id: Optional[str] | None = None
-    title: str
-    message: str
-    is_completed: bool
 
 def calc_timestamp():
     dt = datetime.now()
@@ -40,20 +40,32 @@ def calc_timestamp():
 async def get_to_do():
     with open(path, "r") as reader:
        return reader.read()
-
+    
 @app.post("/to-do")
 async def post_entry(new_entry: To_Do):
     to_do_entry_dict = new_entry.model_dump()
     to_do_entry_dict.update({"id": gen_rando_id()})
     to_do_entry_dict.update({"Created": calc_timestamp()})
     with open(path, "a+") as editor:
-        editor.write(json.dumps(to_do_entry_dict, indent=3))
+        editor.write(json.dumps(to_do_entry_dict, indent=4) + "\n\n")
+    
+@app.get("/to-do/{id}")
+async def get_single_to_to(id: str):
+    pass
+        
+@app.put("/to-do/{id}")
+async def update_to_do(id: str):
+    pass
+
+@app.delete("/to-do/{id}")
+async def delete_to_do(id: str):
+    pass
 
 
 
 
 """
-GET - @app.get("/to-do")
+GET - @app.get("/to-do") - COMPLETED
 GET - @app.get("/to-do/{title}")
 POST - @app.post("/to-do")
 PUT - @app.put("/to-do/{title}")
