e9300a42bccf2b9918ded43ab608ee51bc6bef23
Shaypher Ashe <149912769+shaypheraddison@users.noreply.github.com>
2024-01-31 18:19:37 -0600

    Add files via upload
    Added category to the class. Also updated the GET and DELETE endpoints to be able to find any parameter

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ if not path.is_file():
 
 class To_Do(BaseModel):
     id: Optional[str] | None = None
+    category: str
     title: str
     message: str
     is_completed: bool
@@ -35,30 +36,41 @@ def calc_timestamp():
 
 
 @app.get("/to-do")
-async def get_to_do():
+async def get_to_do(id: Optional[str] | None=None, category: Optional[str] | None=None, title: Optional[str] | None=None, is_completed: Optional[bool] | None=None):
     with open(path, "r") as reader:
-       return json.loads(reader.read())
-    
+        to_do_items = json.loads(reader.read())
+        
+        new_items = []
+        for item in to_do_items:
+            if item and item["id"] == id:
+                return item
+            elif category and item["category"] == category:
+                new_items.append(item)
+            elif title and item["title"] == title:
+                new_items.append(item)
+            elif is_completed is not None and item["is_completed"] == is_completed:
+                new_items.append(item)
+
+        if not any((id, category, title, is_completed)):
+            return to_do_items
+
+        if not new_items:
+            raise HTTPException(status_code=404, detail="Item cannot be found")
+        
+        return new_items
 
 @app.post("/to-do")
 async def post_entry(new_entry: To_Do):
 
-    # create new entry for to-do list
     to_do_entry_dict = new_entry.model_dump()
     to_do_entry_dict.update({"id": gen_rando_id()})
     to_do_entry_dict.update({"Created": calc_timestamp()})
 
-    # read the file to obatin the needed data
-    # loads is used to go from JSON to Python
     with open(path, "r") as reader:
         data = json.loads(reader.read())
-
-    # reference data retrieved from read method above and have it stored in data variable
-    # append the list, data var, with the new post entry 
+ 
     data.append(to_do_entry_dict)
-    
-    # open the file as a writer to re-write all of the data to the file
-    # dumps is used to go from Python to JSON
+
     with open(path, "w") as writer:
         writer.write(json.dumps(data, indent=4))
     return to_do_entry_dict
@@ -68,7 +80,7 @@ async def post_entry(new_entry: To_Do):
 async def get_single_to_to(id: str):
     with open(path, "r") as reader:
         to_do_items = json.loads(reader.read())
-
+        
         for item in to_do_items:
             if item["id"] == id:
                 return item
@@ -78,53 +90,51 @@ async def get_single_to_to(id: str):
         
 @app.put("/to-do/{id}")
 async def update_to_do(id: str, updated_entry: To_Do):
-    # getting data entered in from endpoint, adding in the ID of the endpoint to data and adding completed timestamp
+
     to_do_entry_dict = updated_entry.model_dump()
     to_do_entry_dict["id"] = id
     to_do_entry_dict.update({"Completed": calc_timestamp()})
 
-    # loading the JSON file as a python document and reading the data into a reference-able variable
     with open(path, "r") as reader:
         to_do_items = json.loads(reader.read())
-
-    # iterating through all of the items to find specific ID entered in endpoint and updating the data with entered info
-    # break is used to stop the loop immediately after the item is found and updated 
+    
     for item in to_do_items:
         if item["id"] == id:
             item.update(to_do_entry_dict)
             break
-    # raise a 404 error in case ID is entered in url and cannot be found
+    
     else:
         raise HTTPException (status_code=404, detail="Item not found")
 
-    # opening file as a writer to truncate all of the data with the updated information
-    # dumps python data back into the JSON file and the .dumps converts everything 
     with open(path, "w") as writer:
         writer.write(json.dumps(to_do_items, indent=4))
     
-    # returns this message when item has been updated successfully
     return {"message": "Item has been successfully updated"}
 
 
-@app.delete("/to-do/{id}")
-async def delete_to_do(id: str):
-    # view data as read only
+@app.delete("/to-do")
+async def delete_to_do(id: Optional[str] | None=None, category: Optional[str] | None=None, title: Optional[str] | None=None, is_completed: Optional[bool] | None=None):
+
     with open(path, "r") as reader:
         to_do_items = json.loads(reader.read())
 
-    # iterate through each object and search for ID entered in URL.
-    # once ID is found it removes it from the list and immediately stops the loop with the break
+    new_items = []
     for item in to_do_items:
-        if item["id"] == id:
-            to_do_items.remove(item)
-            break
-    # status code 404 raised if ID cannot be found from file
-    else:
+        if item and item["id"] == id:
+            continue
+        elif category and item["category"] == category:
+            continue
+        elif title and item["title"] == title:
+            continue
+        elif is_completed is not None and item["is_completed"] == is_completed:
+            continue
+        else:
+            new_items.append(item)
+   
+    if not new_items:
         raise HTTPException (status_code=404, detail="Item not found")
 
-    # opens file as a writer to truncate the data removed and dumps in the JSON data back to the file
     with open(path, "w") as writer:
-        writer.write(json.dumps(to_do_items, indent=4))
+        writer.write(json.dumps(new_items, indent=4))
 
-    # returns the message when item has been successfully deleted
-    return {"message": "Item has been successfully deleted."}
+    return {"message": "Deletion completed."}
