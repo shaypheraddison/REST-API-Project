0e72ae6c7d858feaa43e22dd6d478bb524f92173
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-02-12 08:50:56 -0600

    completed delete endpoint

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {mydb.database}")
 mycursor.execute(
-    "CREATE TABLE IF NOT EXISTS things_to_do (id VARCHAR(4), category VARCHAR(20), title VARCHAR(20), message VARCHAR(255), is_completed BOOLEAN, created_timestamp DATETIME, completed_timestamp TIMESTAMP)"
+    "CREATE TABLE IF NOT EXISTS things_to_do (id VARCHAR(4), category VARCHAR(20), title VARCHAR(20), message VARCHAR(255), is_completed BOOLEAN, created_timestamp DATETIME, completed_timestamp DATETIME)"
     )
 mycursor.execute("SET SQL_MODE='ALLOW_INVALID_DATES';")
 
@@ -41,17 +41,10 @@ class To_Do(BaseModel):
     category: str
     title: str
     message: str
-    is_completed: bool
-
-# @app.get("/to-do")
-# async def get_all_to_do():
-#     mycursor.execute("SELECT * FROM things_to_do")
-#     things_to_do = mycursor.fetchall()
-
-#     return things_to_do
+    is_completed: str
 
 @app.get("/to-do")
-async def get_certain_to_do(id: Optional[str] | None=None, category: Optional[str] | None=None, title: Optional[str] | None=None, is_completed: Optional[bool] | None=None):
+async def get_certain_to_do(id: Optional[str] | None=None, category: Optional[str] | None=None, title: Optional[str] | None=None, is_completed: Optional[str] | None=None):
     mycursor.execute("SELECT * FROM things_to_do")
     things_to_do = mycursor.fetchall()
 
@@ -74,75 +67,49 @@ async def get_certain_to_do(id: Optional[str] | None=None, category: Optional[st
 
 @app.post("/to-do")
 async def post_entry(new_entry: To_Do):
-    sql_query = (f"INSERT INTO things_to_do (id, category, title, message, is_completed, created_timestamp) VALUES (%s, %s, %s, %s, False, %s)")
+    sql_query = ("INSERT INTO things_to_do (id, category, title, message, is_completed, created_timestamp) VALUES (%s, %s, %s, %s, %s, %s)")
     mycursor.execute(
-        sql_query, 
-        (gen_rando_id(), 
-        new_entry.category, 
-        new_entry.title,
-        new_entry.message,
-        calc_timestamp())
+        sql_query,
+        (
+            gen_rando_id(), 
+            new_entry.category, 
+            new_entry.title,
+            new_entry.message,
+            new_entry.is_completed,
+            calc_timestamp()
+        )
     )
     mydb.commit()
     return new_entry
 
 @app.delete("/to-do")
-async def delete_to_do(id: Optional[str] | None=None, category: Optional[str] | None=None, title: Optional[str] | None=None, is_completed: Optional[bool] | None=None):
+async def delete_to_do(id: Optional[str] | None=None, category: Optional[str] | None=None, title: Optional[str] | None=None, is_completed: Optional[str] | None=None):
+    sql_query = "DELETE FROM things_to_do WHERE id = %s OR category = %s or is_completed = %s OR title = %s"
+    mycursor.execute(sql_query, (id, category, is_completed, title))
+    mydb.commit()
 
-    with open(path, "r") as reader:
-        to_do_items = json.loads(reader.read())
-
-    new_items = []
-    for item in to_do_items:
-        if item and item[0] == id:
-            continue
-        elif category and item[1] == category:
-            continue
-        elif title and item[2] == title:
-            continue
-        elif is_completed is not None and item[4] == is_completed:
-            continue
-        else:
-            new_items.append(item)
-   
-    if not new_items:
-        raise HTTPException (status_code=404, detail="Item not found")
-
-    with open(path, "w") as writer:
-        writer.write(json.dumps(new_items, indent=4))
-
-    return {"message": "Deletion completed."}
+    return "Deletion Completed."
 
 
 @app.get("/to-do/{id}")
 async def get_single_to_to(id: str):
-    mycursor.execute("SELECT * FROM things_to_do")
-    things_to_do = mycursor.fetchall()
-
-    for thing in things_to_do:
-        if thing[0] == id:
-            return thing
-    mycursor.close()
+    sql_query = "SELECT * FROM things_to_do WHERE id = %s"
+    return mycursor.execute(sql_query, (id,))
+    
             
 @app.put("/to-do/{id}")
 async def update_to_do(id: str, updated_entry: To_Do):
-
-    to_do_entry_dict = updated_entry.model_dump()
-    to_do_entry_dict["id"] = id
-    to_do_entry_dict.update({"Completed": calc_timestamp()})
-
-    with open(path, "r") as reader:
-        to_do_items = json.loads(reader.read())
-    
-    for item in to_do_items:
-        if item["id"] == id:
-            item.update(to_do_entry_dict)
-            break
-    
-    else:
-        raise HTTPException (status_code=404, detail="Item not found")
-
-    with open(path, "w") as writer:
-        writer.write(json.dumps(to_do_items, indent=4))
-    
-    return {"message": "Item has been successfully updated"}
+    sql_query = ("UPDATE things_to_do SET category = %s, title = %s, message = %s, is_completed = %s, completed_timestamp = %s WHERE id = %s")
+    mycursor.execute(
+        sql_query, 
+        (
+            updated_entry.category, 
+            updated_entry.title, 
+            updated_entry.message, 
+            updated_entry.is_completed, 
+            calc_timestamp(), 
+            id
+        )
+    )
+    mydb.commit()
+    return updated_entry
